1885392258b0cd1f380bc4a976e2a1829ae56c94
Артём Семенченко <artem_991456@mail.ru>
2025-05-06 12:26:43 +0300

    feat: add Spring Boot setup with PostgreSQL

---
+++ rest-api/src/main/java/ru/semenchenko/project/rest_api/RestApiProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.semenchenko.project.rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiProjectApplication.class, args);
+	}
+
+}
