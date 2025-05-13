96dc5d4e6bf2dcf889896b18a15904d3d3ef6815
Артём Семенченко <artem_991456@mail.ru>
2025-05-13 15:31:45 +0300

    chore: initialize client

---
+++ rest-api-client/src/test/java/ru/semenchenko/client/rest_api_client/RestApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.semenchenko.client.rest_api_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
