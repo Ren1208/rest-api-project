4ceb77f3417dcd6a33e6a5ba72f611a99c03ffd5
Артём Семенченко <artem_991456@mail.ru>
2025-05-13 13:43:59 +0300

    feat: add Measurement validation

--- rest-api/src/main/java/ru/semenchenko/project/rest_api/util/MeasurementValidator.java
+++ rest-api/src/main/java/ru/semenchenko/project/rest_api/util/MeasurementValidator.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import ru.semenchenko.project.rest_api.models.Measurement;
+import ru.semenchenko.project.rest_api.services.SensorsService;
 
 /**
  * @author Artyom Semenchenko
@@ -11,6 +12,13 @@ import ru.semenchenko.project.rest_api.models.Measurement;
 
 @Component
 public class MeasurementValidator implements Validator {
+
+    private final SensorsService sensorsService;
+
+    public MeasurementValidator(SensorsService sensorsService) {
+        this.sensorsService = sensorsService;
+    }
+
     @Override
     public boolean supports(Class<?> clazz) {
         return Measurement.class.equals(clazz);
@@ -19,6 +27,15 @@ public class MeasurementValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         Measurement measurement = (Measurement) target;
+
+        if (measurement.getSensor() == null) {
+            return;
+        }
+
+        if (sensorsService.getSensorByName(measurement.getSensor().getName()).isEmpty()) {
+            errors.rejectValue("sensor",
+                    "Сенсор с таким названием не зарегистрирован!");
+        }
     }
 
 }
