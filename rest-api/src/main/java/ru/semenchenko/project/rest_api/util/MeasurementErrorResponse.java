2ea8d5473bfd550d2bb0975e776371a39523f2df
Артём Семенченко <artem_991456@mail.ru>
2025-05-13 13:00:46 +0300

    feat: add Measurement exceptions

---
+++ rest-api/src/main/java/ru/semenchenko/project/rest_api/util/MeasurementErrorResponse.java
@@ -0,0 +1,32 @@
+package ru.semenchenko.project.rest_api.util;
+
+
+/**
+ * @author Artyom Semenchenko
+ */
+
+public class MeasurementErrorResponse {
+    private String message;
+    private long timestamp;
+
+    public MeasurementErrorResponse(String message, long timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
---
+++ rest-api/src/main/java/ru/semenchenko/project/rest_api/util/MeasurementException.java
@@ -0,0 +1,11 @@
+package ru.semenchenko.project.rest_api.util;
+
+/**
+ * @author Artyom Semenchenko
+ */
+
+public class MeasurementException extends RuntimeException {
+    public MeasurementException(String msg) {
+        super(msg);
+    }
+}
