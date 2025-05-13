1ecc3ac05f8df8e9c55cda3aec4e21b5748c78f4
Артём Семенченко <artem_991456@mail.ru>
2025-05-13 12:39:35 +0300

    feat: add getSensorByName in SensorsService

--- rest-api/src/main/java/ru/semenchenko/project/rest_api/repositories/SensorRepository.java
+++ rest-api/src/main/java/ru/semenchenko/project/rest_api/repositories/SensorRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.semenchenko.project.rest_api.models.Sensor;
 
+import java.util.Optional;
+
 /**
  * @author Artyom Semenchenko
  */
 
 @Repository
 public interface SensorRepository extends JpaRepository<Sensor, String> {
+    Optional<Sensor> findByName(String name);
 }
--- rest-api/src/main/java/ru/semenchenko/project/rest_api/services/SensorsService.java
+++ rest-api/src/main/java/ru/semenchenko/project/rest_api/services/SensorsService.java
@@ -2,6 +2,10 @@ package ru.semenchenko.project.rest_api.services;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.semenchenko.project.rest_api.models.Sensor;
+import ru.semenchenko.project.rest_api.repositories.SensorRepository;
+
+import java.util.Optional;
 
 /**
  * @author Artyom Semenchenko
@@ -10,4 +14,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(readOnly = true)
 public class SensorsService {
+
+    private final SensorRepository sensorRepository;
+
+    public SensorsService(SensorRepository sensorRepository) {
+        this.sensorRepository = sensorRepository;
+    }
+
+    public Optional<Sensor> getSensorByName(String name) {
+        return sensorRepository.findByName(name);
+    }
 }
