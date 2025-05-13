31f834322042427a5dde8fcdfc7fb8771fa21fe6
Артём Семенченко <artem_991456@mail.ru>
2025-05-13 13:45:47 +0300

    feat: add enrichMeasurement and save in MeasurementService

--- rest-api/src/main/java/ru/semenchenko/project/rest_api/services/MeasurementsService.java
+++ rest-api/src/main/java/ru/semenchenko/project/rest_api/services/MeasurementsService.java
@@ -2,6 +2,10 @@ package ru.semenchenko.project.rest_api.services;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.semenchenko.project.rest_api.models.Measurement;
+import ru.semenchenko.project.rest_api.repositories.MeasurementRepository;
+
+import java.time.LocalDateTime;
 
 /**
  * @author Artyom Semenchenko
@@ -10,4 +14,24 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(readOnly = true)
 public class MeasurementsService {
+
+    private final MeasurementRepository measurementRepository;
+    private final SensorsService sensorsService;
+
+    public MeasurementsService(MeasurementRepository measurementRepository, SensorsService sensorsService) {
+        this.measurementRepository = measurementRepository;
+        this.sensorsService = sensorsService;
+    }
+
+    @Transactional
+    public void add(Measurement measurement) {
+        enrichMeasurement(measurement);
+        measurementRepository.save(measurement);
+    }
+
+    private void enrichMeasurement(Measurement measurement) {
+        measurement.setMeasurementTime(LocalDateTime.now());
+        measurement.setSensor(sensorsService.getSensorByName(measurement.getSensor().getName()).get());
+    }
+
 }
