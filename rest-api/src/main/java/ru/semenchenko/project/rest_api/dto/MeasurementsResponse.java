5ec240e877de06e565d47c8ccdca6dd3a5a6e2af
Артём Семенченко <artem_991456@mail.ru>
2025-05-13 14:18:13 +0300

    feat: add MeasurementsResponse wrapper for List<Measurement>

---
+++ rest-api/src/main/java/ru/semenchenko/project/rest_api/dto/MeasurementsResponse.java
@@ -0,0 +1,23 @@
+package ru.semenchenko.project.rest_api.dto;
+
+import java.util.List;
+
+/**
+ * @author Artyom Semenchenko
+ */
+
+public class MeasurementsResponse {
+    private List<MeasurementDTO> measurements;
+
+    public MeasurementsResponse(List<MeasurementDTO> measurements) {
+        this.measurements = measurements;
+    }
+
+    public List<MeasurementDTO> getMeasurements() {
+        return measurements;
+    }
+
+    public void setMeasurements(List<MeasurementDTO> measurements) {
+        this.measurements = measurements;
+    }
+}
